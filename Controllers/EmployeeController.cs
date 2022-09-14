aa61818082b4185c25b9eb0dc2816554acee0af9
1821050999@student.humg.edu.vn <1821050999@student.humg.edu.vn>
2022-09-14 20:08:21 +0700

    NguyenTungSon_1409

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+/*using Microsoft.AspNetCore.Mvc;
 
 namespace DemoMVC.Controllers
 {
@@ -14,5 +14,33 @@ namespace DemoMVC.Controllers
             ViewBag.name = "Hello" + EmployeeID + "-" + EmployeeName + "-" + Age;
             return View();
         }
+    }
+}*/
+using System.Security.Principal;
+using Microsoft.AspNetCore.Mvc;
+using DemoMVC.Models;
+namespace DemoMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Employee> EmployeeList = new List<Employee>()
+            {
+                new Employee {EmployeeID = 1, EmployeeName = "Nguyen Van A", Age = 18},
+                new Employee {EmployeeID = 2, EmployeeName = "Nguyen Van B", Age = 18},
+                new Employee {EmployeeID = 3, EmployeeName = "Nguyen Van C", Age = 18},
+                new Employee {EmployeeID = 4, EmployeeName = "Nguyen Van D", Age = 18},
+                new Employee {EmployeeID = 5, EmployeeName = "Nguyen Van E", Age = 18},
+            };
+            ViewData["Employees"] = EmployeeList;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Employee EmployeeList)
+        {
+            return View();
+        } 
+        
     }
 }
--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -1,18 +1,28 @@
+using System.Security.Principal;
 using Microsoft.AspNetCore.Mvc;
-
+using DemoMVC.Models;
 namespace DemoMVC.Controllers
 {
     public class PersonController : Controller
     {
         public IActionResult Index()
         {
+            List<Person> PersonList = new List<Person>()
+            {
+                new Person {PersonID = 1, PersonName = "Nguyen Van A", Age = 18},
+                new Person {PersonID = 2, PersonName = "Nguyen Van B", Age = 18},
+                new Person {PersonID = 3, PersonName = "Nguyen Van C", Age = 18},
+                new Person {PersonID = 4, PersonName = "Nguyen Van D", Age = 18},
+                new Person {PersonID = 5, PersonName = "Nguyen Van E", Age = 18},
+            };
+            ViewData["Persons"] = PersonList;
             return View();
         }
         [HttpPost]
-        public IActionResult Index(int PersonID, string PersonName, int Age)
+        public IActionResult Index(Person PersonList)
         {
-            ViewBag.name = "Hello" + PersonID + "-" + PersonName + "-" + Age;
             return View();
-        }
+        } 
+        
     }
 }
